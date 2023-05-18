8f3ced63b9412fb021e49deb2c67796ca376307f
hsiaoseanhs <hsiaoseanhs@gmail.com>
2023-05-18 19:22:16 +0800

    output txt

--- Easyocr/test.py
+++ Easyocr/test.py
@@ -1,5 +1,9 @@
 import easyocr
 reader = easyocr.Reader(['ja'], gpu=False) # this needs to run only once to load the model into memory
-result = reader.readtext('Training_data/test/japanese_hiragana.jpg', detail = 0)
+result = reader.readtext('Training_data/test/japanese.jpg')#, detail = 0)
 
-print(result)
+f = open('EasyOCR/result.txt', 'w')
+for item in result:
+	f.write(str(item) + "\n")
+f.close()
+print(type(result))
