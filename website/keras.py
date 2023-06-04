78946c343d00529882e865d9bd4637b042725cfb
hsiaoseanhs <hsiaoseanhs@gmail.com>
2023-06-05 00:07:50 +0800

    nothing

--- website/keras.py
+++ website/keras.py
@@ -1,3 +1,4 @@
+from keras import models
 from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
