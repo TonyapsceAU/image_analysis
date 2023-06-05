b8f85872570f1e8ced078849f02338d7a9b54fc1
TonyapsceAU <91184409+TonyapsceAU@users.noreply.github.com>
2023-06-05 20:28:34 +0800

    Update ImageQuadtree.py

--- websitev2/ImageQuadtree.py
+++ websitev2/ImageQuadtree.py
@@ -4,60 +4,44 @@ import os
 class Quadtree:
     def __init__(self):
         self.images = []
-        self.divided = False
-        self.cuts = []
     
-    def check_img(self,path,image):
+    def check_img(self, path, image):
         w = image.width
         h = image.height
-        lengh = 150
-        if check_word(image)>0.5 or w<=lengh or h<=lengh:
-            w, h = image.size
-            self.images.append((image,path[len("temporary/"):]) )
-        else :
-            images = self.subdivide(image)
-            end = str(images[0].width) + "x" + str(images[0].height) + ".jpg"
-            for i in range(0,len(images)):
-                img_path = (path +  str(i) + "_" + end)
-                images[i].save(img_path)
-                img = Image.open(img_path)
-                self.cuts[i].check_img(path+str(i) + "_", img)
-                os.remove(img_path)
-                
+        lengh = 75
+        image.save(path + ".jpg")
+        Stack = [path]
+        while Stack:
+            current_path = Stack.pop()
+            current_image = Image.open(current_path+".jpg")
+            w, h = current_image.size
+            
+            if check_word(current_image) > 0.5 or w <= lengh or h <= lengh:
+                self.images.append(current_path[len("temporary/"):]+".jpg") 
+            else:
+                devided_images = self.subdivide(current_image)
+                for i in range(0,len(devided_images)):
+                    newimg_path = (current_path + str(i) + "_")
+                    devided_images[i].save(newimg_path + ".jpg")
+                    Stack.append(newimg_path)
+                    
+                os.remove(current_path+".jpg")
+        
             
 
     def subdivide(self,image):
         w, h = image.size
-
-        t0 = Quadtree()
-        t1 = Quadtree()
-        t2 = Quadtree()
-        t3 = Quadtree()
-        self.cuts = [t0,t1,t2,t3]
-
-        self.divided = True
         
-        images = []
-        images.append(image.crop((w/2, 0,w,h/2)))
-        images.append(image.crop((0, 0, w/2, h/2)))
-        images.append(image.crop((w/2, h/2, w, h)))
-        images.append(image.crop((0, h/2, w/2,h)))
+        new_images = []
+        new_images.append(image.crop((w/2, 0,w,h/2)))
+        new_images.append(image.crop((0, 0, w/2, h/2)))
+        new_images.append(image.crop((w/2, h/2, w, h)))
+        new_images.append(image.crop((0, h/2, w/2,h)))
         
-        return images
+        return new_images
     
     def get_images(self):
-        images = []
-        if self.divided:
-            for tree in self.cuts:
-                temp = tree.get_images()
-                if temp:
-                    images += temp
-            
-        else:
-            if self.images:
-                images = self.images
-
-        return images
+        return self.images
 
 
 def check_word(image):
