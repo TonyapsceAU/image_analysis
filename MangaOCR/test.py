8b6fef18dd44d54f2032daed29a4e9af3b42c5b7
hsiaoseanhs <hsiaoseanhs@gmail.com>
2023-06-07 03:33:53 +0800

    copy

--- MangaOCR/test.py
+++ MangaOCR/test.py
@@ -2,6 +2,6 @@ import PIL.Image
 from manga_ocr import MangaOcr
 
 mocr = MangaOcr()
-img = PIL.Image.open("Training_data/test/japanese_small.jpg")
+img = PIL.Image.open("Training_data/test/english.jpg")
 text = mocr(img)
 print(text)
